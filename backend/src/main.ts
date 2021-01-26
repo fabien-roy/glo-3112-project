ee6623db43520a6f1438bc2ff715e1c8dfea6cdd
Fabien Roy <fabienroy28@gmail.com>
2021-01-26 16:45:30 -0500

    Add tsoa

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import { RegisterRoutes } from './routes/routes';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+RegisterRoutes(app);
 
 app.listen(port, () => console.log(`Server started listening to port ${port}`));
