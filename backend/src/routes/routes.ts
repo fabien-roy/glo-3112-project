7d8311cc23f93b761b2272072949f58752b18767
Fabien Roy <fabienroy28@gmail.com>
2021-01-26 16:55:11 -0500

    Fix tsoa swagger

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -19,7 +19,7 @@ export function RegisterRoutes(app: express.Router) {
     //  NOTE: If you do not see routes for all of your controllers in this file, then you might not have informed tsoa of where to look
     //      Please look into the "controllerPathGlobs" config option described in the readme: https://github.com/lukeautry/tsoa
     // ###########################################################################################################
-        app.get('/api/v1',
+        app.get('/',
             function (request: any, response: any, next: any) {
             const args = {
             };
@@ -40,7 +40,7 @@ export function RegisterRoutes(app: express.Router) {
             promiseHandler(controller, promise, response, undefined, next);
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/api/v1/msg',
+        app.get('/msg',
             function (request: any, response: any, next: any) {
             const args = {
             };
