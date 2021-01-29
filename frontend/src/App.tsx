753214c8d4fd0bc01e16c877d7ab43ffbf01c6dc
Annie Auclair <annie@shimadzu.ca>
2021-01-29 18:54:48 -0500

    - linting, clean-up - router, material-ui/styles package addition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-//import logo from './logo.svg';
 import './App.css';
 import { Navigation } from './components/Navigation';
 
-
 const theme = createMuiTheme({
   typography: {
-    fontFamily: [
-      'Rock Salt',
-      'cursive',
-    ].join(','),
-  }
+    fontFamily: ['Rock Salt', 'cursive'].join(','),
+  },
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-          <Navigation/> 
+        <Navigation />
       </div>
     </ThemeProvider>
   );
