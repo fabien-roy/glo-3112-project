6dc5efcdb076a20e21cf9e36340f331ba59441e2
Annie Auclair <annie@shimadzu.ca>
2021-01-30 12:32:29 -0500

    - remove no used App.css - node-sass package addition - rename index.css to scss - added Navigation.test.tsx for future tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import './App.css';
 import { Navigation } from './components/Navigation';
 
 const theme = createMuiTheme({
