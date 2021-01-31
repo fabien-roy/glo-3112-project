9cd68515e084d32da475bd1620097581fd06b45a
Fabien Roy <fabienroy28@gmail.com>
2021-01-31 14:57:06 -0500

    Add simple test to user controller

---
+++ backend/src/controllers/users.controller.test.ts
@@ -0,0 +1,23 @@
+import { UsersController } from './users.controller';
+
+const fakeUser = {
+  username: 'username',
+};
+
+jest.mock('../services/users.service', () => ({
+  UsersService: class {
+    public get(username: string) {
+      return username === fakeUser.username ? fakeUser : null;
+    }
+  },
+}));
+
+describe('When getting user by username', () => {
+  it('should retrieve user from service', () => {
+    const usersController = new UsersController();
+
+    return usersController.get(fakeUser.username).then((user) => {
+      expect(user).toBe(fakeUser);
+    });
+  });
+});
