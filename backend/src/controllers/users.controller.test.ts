e4ae4f883c29bf736f2d3126e9df1e6543a35f36
Fabien Roy <fabienroy28@gmail.com>
2021-01-31 17:04:55 -0500

    Implement fake factory

--- backend/src/controllers/users.controller.test.ts
+++ backend/src/controllers/users.controller.test.ts
@@ -1,5 +1,7 @@
 import { UsersController } from './users.controller';
-import { fakeUser } from '../util/users.factory';
+import { UserFactory } from '../util/users.factory';
+
+const fakeUser = UserFactory.make();
 
 jest.mock('../services/users.service', () => ({
   UsersService: class {
--- backend/src/util/users.factory.ts
+++ backend/src/util/users.factory.ts
@@ -1,4 +1,12 @@
-// TODO : Use node-factory (with type used by both frontend and backend)
-export const fakeUser = {
-  username: 'username',
-};
+import { factory } from 'node-factory';
+import { User } from '../types/users';
+
+export const UserFactory = factory<User>((fake) => ({
+  username: fake.internet.userName(),
+  email: fake.internet.email(),
+  phoneNumber: fake.phone.phoneNumber(),
+  firstName: fake.name.firstName(),
+  lastName: fake.name.lastName(),
+  description: fake.lorem.words(10),
+  avatarReference: fake.random.uuid(),
+}));
