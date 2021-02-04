5ae9c5194a6ff76f661a697a258ecddb9df78ad5
Maxime Caron <maximecaron321@gmail.com>
2021-02-03 19:02:15 -0500

    adds the Post component, linting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,6 +11,6 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 reportWebVitals();
--- frontend/src/views/posts/Post.tsx
+++ frontend/src/views/posts/Post.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Feed = () => {
+export const Post = () => {
   return (
     <div>
-      <h1> Feed view!</h1>
+      <h1>Post view!</h1>
       <Link to="/"> Back to home </Link>
     </div>
   );
 };
-export default Feed;
+export default Post;
