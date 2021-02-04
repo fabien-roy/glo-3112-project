cccb406ffbbe384e1600a4bbdf498197d57ef527
Fabien Roy <fabienroy28@gmail.com>
2021-02-04 10:28:27 -0500

    Only use react-router-dom

--- frontend/src/views/posts/Post.tsx
+++ frontend/src/views/posts/Post.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router-dom';
 
 interface ParamTypes {
   postId: string;
