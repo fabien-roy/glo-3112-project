538e8a19f5e60fd26485aed6b6658c094e60fc63
Maxime Caron <maximecaron321@gmail.com>
2021-02-04 11:45:56 -0500

    Changes from MemoryRouter to BrowserRouter in App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders App', () => {
-  render(<MemoryRouter><App /></MemoryRouter>);
+  render(<BrowserRouter><App /></BrowserRouter>);
 });
