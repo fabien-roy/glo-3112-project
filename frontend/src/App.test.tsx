1aa678b54ce8467746d8882133b958b8b9c4f72c
Maxime Caron <maximecaron321@gmail.com>
2021-02-04 11:54:16 -0500

    Fix App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders App', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
+  render(<App />);
 });
