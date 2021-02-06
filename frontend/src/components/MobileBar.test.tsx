576a7815828322a77ced4113e9787bc36b1a465b
Annie Auclair <annie@shimadzu.ca>
2021-02-05 20:00:00 -0500

    fixed syntax render tests

--- frontend/src/components/MobileBar.test.tsx
+++ frontend/src/components/MobileBar.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import MobileBar from './MobileBar';
+import { MobileBar } from './MobileBar';
 
 test('renders MobileBat', () => {
   render(<MobileBar />);
