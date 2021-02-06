52787032779156a90a9c9e8ff8d89a24c5365cc7
Fabien Roy <fabienroy28@gmail.com>
2021-02-06 11:27:27 -0500

    Move theme to layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { purple } from '@material-ui/core/colors';
-import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from 'router/Router';
 import { routes } from 'router/Config';
 import MainLayout from './layouts/MainLayout';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: purple,
-  },
-  typography: {
-    fontFamily: ['Noto+Sans', 'sans-serif'].join(','),
-  },
-});
-
 function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <MainLayout>
-          <Router routes={routes} />
-        </MainLayout>
-      </ThemeProvider>
+      <MainLayout>
+        <Router routes={routes} />
+      </MainLayout>
     </BrowserRouter>
   );
 }
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,16 +1,29 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import { Container } from '@material-ui/core';
 import { Navigation } from '../components/Navigation';
 
 interface ParamTypes {
-  children: ReactNode;
+  children: any;
 }
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+  },
+  typography: {
+    fontFamily: ['Noto+Sans', 'sans-serif'].join(','),
+  },
+});
 
 export const MainLayout = ({ children }: ParamTypes) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Navigation />
-      {children}
-    </>
+      <Container>{children}</Container>
+    </ThemeProvider>
   );
 };
 
