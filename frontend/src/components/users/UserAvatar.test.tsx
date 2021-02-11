8ece1a3dfc2f15698fe2cc21f116d00cb06a453a
AkroutOmar <35777493+AkroutOmar@users.noreply.github.com>
2021-02-10 19:28:56 -0500

    fix test errors

--- frontend/src/components/users/UserAvatar.test.tsx
+++ frontend/src/components/users/UserAvatar.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import { Avatar } from '@material-ui/core';
 import UserAvatar from './UserAvatar';
