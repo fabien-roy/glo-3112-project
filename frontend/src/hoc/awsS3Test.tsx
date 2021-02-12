f9fdff55c4b91960941359bccd265da603131b43
Omar <omar.akrout.1@ulaval.ca>
2021-02-12 16:22:03 -0500

    delete useless test file

--- frontend/src/hoc/awsS3Test.tsx
+++
@@ -1,8 +0,0 @@
-// TODO: write awsS3Connect test
-// uses cases:
-// - return the passed components
-// - when uploadFile is called should fire S3FileUpload
-// - if uploadFile went through should setState(src)
-// - otherwise should log error
-
-export {};
