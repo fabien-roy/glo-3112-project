79796d6cb4e958bce921ded7732c78be7eca6521
Petros Fytilis <petros.fytilis.1@ulaval.ca>
2021-02-12 23:14:45 -0500

    Implements API endpoint for user modification Refactors some naming for consistency

--- backend/src/types/users.ts
+++ backend/src/types/users.ts
@@ -10,10 +10,19 @@ export interface User {
   avatarReference?: string;
 }
 
-export interface UserCreationRequest {
+export interface UserCreationParams {
   username: string;
   email: string;
   phoneNumber: string;
   firstName: string;
   lastName: string;
+}
+
+export interface UserModificationParams {
+  email?: string;
+  phoneNumber?: string;
+  firstName?: string;
+  lastName?: string;
+  description?: string;
+  avatarReference?: string;
 }
