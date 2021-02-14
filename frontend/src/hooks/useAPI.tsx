527b13a897d9e697fd3eedfeab8c46fd046594ee
Fabien Roy <fabienroy28@gmail.com>
2021-02-14 10:45:29 -0500

    Generify useAPI hook

---
+++ frontend/src/hooks/useAPI.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import APIService from 'services/APIService';
+
+export default function useAPI(method, setData, ...params) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = () => {
+    setIsLoading(true);
+
+    APIService[method](...params)
+      .then((response) => setData(response.data))
+      .catch((err) => setError(err))
+      .finally(() => setIsLoading(false));
+  };
+
+  // TODO : Fix exhaustive-deps warning
+  useEffect(() => fetchData(), []);
+
+  return [isLoading, error, fetchData];
+}
--- frontend/src/hooks/useGetPosts.tsx
+++ frontend/src/hooks/useGetPosts.tsx
@@ -1,24 +1,11 @@
-import { useState, useEffect } from 'react';
-import APIService from '../services/APIService';
+import { useState } from 'react';
 import { Post } from '../types/posts';
+import useAPI from './useAPI';
 
 export default function useGetPosts() {
   const [posts, setPosts] = useState<Post[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // TODO : Can we generify this into another hook?
-  const retrievePosts = () => {
-    setIsLoading(true);
-
-    APIService.getPosts()
-      .then((response) => setPosts(response.data))
-      .catch((err) => setError(err))
-      .finally(() => setIsLoading(false));
-  };
-
-  useEffect(() => retrievePosts(), []);
+  const [isLoading, error, fetchData] = useAPI('getPosts', setPosts);
 
   // TODO : Use everything in the hook or remove them
-  return [posts, isLoading, error, retrievePosts];
+  return [posts, isLoading, error, fetchData];
 }
