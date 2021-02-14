2df4c35c75b81e4cc766ee11f1558c4c11952f30
Fabien Roy <fabienroy28@gmail.com>
2021-02-14 16:21:37 -0500

    Enhance retry datavase connection

--- backend/src/connect.database.ts
+++ backend/src/connect.database.ts
@@ -17,23 +17,24 @@ const retryTimeoutInMilliseconds = 5000;
 const db = mongoose.connection;
 
 db.on('connecting', () => console.log('Connecting to MongoDB...'));
-
-db.on('error', (error) => {
-  console.error('Error in MongoDB connection : ' + error);
-  mongoose.disconnect();
-});
-
+db.on('error', (error) =>
+  console.error(`Error in MongoDB connection : ${error}`),
+);
 db.on('connected', () => console.log('MongoDB connected!'));
-
 db.once('open', () => console.log('MongoDB connection opened!'));
-
 db.on('reconnected', () => console.log('MongoDB reconnected!'));
-
 db.on('disconnected', () => {
-  console.log('MongoDB disconnected! - Retrying in 5 seconds');
-  setTimeout(connectDatabase, retryTimeoutInMilliseconds);
+  console.log('MongoDB disconnected!');
+  retryConnectionAfterTimeout();
 });
+
+const retryConnectionAfterTimeout = () => {
+  console.log('Retrying connection in 5 seconds');
+  setTimeout(connectDatabase, retryTimeoutInMilliseconds);
+};
 
 export function connectDatabase() {
-  mongoose.connect(mongoURL, mongoOptions);
+  mongoose.connect(mongoURL, mongoOptions).catch(() => {
+    retryConnectionAfterTimeout();
+  });
 }
