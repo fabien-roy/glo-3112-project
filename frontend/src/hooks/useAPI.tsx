d6446996d496123fbc2f53346415f1e88cb03d67
Fabien Roy <fabienroy28@gmail.com>
2021-02-14 11:20:05 -0500

    Correct warning disable

--- frontend/src/hooks/useAPI.tsx
+++ frontend/src/hooks/useAPI.tsx
@@ -15,7 +15,7 @@ export default function useAPI(method, setData, ...params) {
   };
 
   // TODO : Fix exhaustive-deps warning
-  // eslint-disable-next line exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => fetchData(), []);
 
   return [isLoading, error, fetchData];
