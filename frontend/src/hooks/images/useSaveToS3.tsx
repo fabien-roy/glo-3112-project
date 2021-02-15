e84bd53c12e52f6992d7fe7a5840dd004434a9c1
Fabien Roy <fabienroy28@gmail.com>
2021-02-15 11:00:25 -0500

    Rename hook

--- frontend/src/hooks/images/useSaveToS3.tsx
+++ frontend/src/hooks/images/useSaveToS3.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import S3Service from 'services/S3Service';
 
 // TODO : Test this correctly
-export default function useUploadToS3(file, dirName) {
+export default function useSaveToS3(file, dirName) {
   const [reference, setReference] = useState(null);
   const [error, setError] = useState(null);
 
+  // TODO : Do we have to use useEffect (like in useAPI?)
   S3Service.uploadFile(file, dirName)
     .then((response) => setReference(response))
     .catch((err) => setError(err));
