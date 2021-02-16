452ca2faa9a651ef25ef3460c46945b46b2641ed
Omar <omar.akrout.1@ulaval.ca>
2021-02-16 15:09:22 -0500

    delete credentials

--- frontend/src/services/S3Service.tsx
+++ frontend/src/services/S3Service.tsx
@@ -1,17 +1,10 @@
 import S3 from 'react-s3';
 
-// const config = {
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   bucketName: process.env.S3_BUCKET_NAME,
-//   region: process.env.S3_BUCKET_REGION,
-// };
-
 const config = {
-  accessKeyId: 'AKIAYQHAIOAOECPWAJPT',
-  secretAccessKey: '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',
-  bucketName: 'test--omar',
-  region: 'ca-central-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  bucketName: process.env.S3_BUCKET_NAME,
+  region: process.env.S3_BUCKET_REGION,
 };
 
 const uploadFile = (file, dirName) => {
