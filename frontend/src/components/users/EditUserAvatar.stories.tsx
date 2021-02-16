164cae6655aa36c769e5655b75be01520a4d998c
Omar <omar.akrout.1@ulaval.ca>
2021-02-16 14:53:32 -0500

    index on uploadHook: d0aae36 delete useless hoc

--- frontend/src/components/users/EditUserAvatar.stories.tsx
+++ frontend/src/components/users/EditUserAvatar.stories.tsx
@@ -15,12 +15,10 @@ const Template: Story<EditUserAvatarProps> = ({ ...args }) => (
 export const WithoutAvatar = Template.bind({});
 WithoutAvatar.args = {
   username: 'Test',
-  onUpload: () => action('onUpload'),
 };
 
 export const WithAvatar = Template.bind({});
 WithAvatar.args = {
   username: 'Test',
-  onUpload: () => action('onUpload'),
   src: 'https://picsum.photos/200',
 };
