9ec01c2ea01a008bfba47cf46f446230b28dfc2e
Annie Auclair <annie@shimadzu.ca>
2021-02-15 22:43:06 -0500

    SearchBar dropdown getting data from backend

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -6,152 +6,43 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { InputAdornment } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import { User } from 'types/users';
+import useGetUsers from '../hooks/useGetUsers';
 
-import { User } from '../views/users/UserProps';
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   input: {
     backgroundColor: 'white',
     height: '35px',
   },
 }));
 
-/* function sleep(delay = 0) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  });
-} */
-
 export function SearchBar() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [options, setOptions] = React.useState<User[]>([]);
-  const loading = open && options.length === 0;
   const [value, setValue] = React.useState<User | null>(null);
-  const [inputValue, setInputValue] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
 
-  React.useEffect(() => {
-    let active = true;
-
-    if (!loading) {
-      return undefined;
-    }
-
-    const users: User[] = [
-      {
-        username: 'Annie',
-        email: 'TestUser1@ugram.com',
-        phoneNumber: '450-666-7777',
-        firstName: 'Test',
-        lastName: 'User1',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-      {
-        username: 'AnnieA',
-        email: 'TestUser1@ugram.com',
-        phoneNumber: '450-666-7777',
-        firstName: 'Test',
-        lastName: 'User1',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-      {
-        username: 'AnnieAA',
-        email: 'TestUser1@ugram.com',
-        phoneNumber: '450-666-7777',
-        firstName: 'Test',
-        lastName: 'User1',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-      {
-        username: 'ArianeTest',
-        email: 'TestUser1@ugram.com',
-        phoneNumber: '450-666-7777',
-        firstName: 'Test',
-        lastName: 'User1',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-      {
-        username: 'JoeBlo',
-        email: 'TestUser2@ugram.com',
-        phoneNumber: '450-664-7777',
-        firstName: 'Test',
-        lastName: 'User2',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-      {
-        username: 'blackdiamond_4',
-        email: 'TestUser2@ugram.com',
-        phoneNumber: '450-664-7777',
-        firstName: 'Test',
-        lastName: 'User2',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-      {
-        username: 'nataliegaron',
-        email: 'TestUser2@ugram.com',
-        phoneNumber: '450-664-7777',
-        firstName: 'Test',
-        lastName: 'User2',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-      {
-        username: 'donaldtrump',
-        email: 'TestUser2@ugram.com',
-        phoneNumber: '450-664-7777',
-        firstName: 'Test',
-        lastName: 'User2',
-        description: '',
-        avatarReference:
-          'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-      },
-    ];
-
-    // (async () => {
-    // const response = await fetch(
-    //  'https://country.register.gov.uk/records.json?page-size=5000'
-    // );
-    // sleep(3000); // For demo purposes.
-    // const users = await response.json();
-
-    if (active) {
-      setOptions(Object.keys(users).map((key) => users[key]) as User[]);
-    }
-    // })();
-
-    return () => {
-      active = false;
-    };
-  }, [loading]);
+  const [users] = useGetUsers();
 
   React.useEffect(() => {
     if (!open) {
       setOptions([]);
+    } else if (Array.isArray(users) && users.length > 0) {
+      setOptions(Object.keys(users).map((key) => users[key]) as User[]);
+    } else {
+      setLoading(true);
     }
-  }, [open]);
+  }, [open, users]);
 
   const history = useHistory();
   const handleInputChange = (user: string) => {
-    setInputValue(user);
     const currentUser = user;
     if (currentUser !== '') {
       const currenRoute = `/users/${currentUser}`;
       history.push(currenRoute);
       setOptions([]);
-      setInputValue('');
+      setValue(null);
     }
   };
 
@@ -173,6 +64,7 @@ export function SearchBar() {
           handleInputChange(newValue.username);
         }
       }}
+      inputValue=""
       getOptionLabel={(option) => option.username}
       options={options}
       loading={loading}
