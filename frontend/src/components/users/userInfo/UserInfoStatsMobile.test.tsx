c4e78e3be7d62890624ae66736a43e0bfa8cb1b6
Omar <omar.akrout.1@ulaval.ca>
2021-02-17 18:20:11 -0500

    fix tests

--- frontend/src/components/users/userInfo/UserInfoStatsMobile.test.tsx
+++ frontend/src/components/users/userInfo/UserInfoStatsMobile.test.tsx
@@ -1,39 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-// import { mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Box from '@material-ui/core/Box';
-// import { UserInfoStatsMobile } from './UserInfoStatsMobile';
-
-// const stats = {
-//   totalPost: 45,
-//   totalFollowers: 450,
-//   totalFollowing: 789,
-// };
+import { UserInfoStatsMobile } from './UserInfoStatsMobile';
 
 describe('When rendering UserInfoStatsMobile', () => {
-  // const wrapper = mount(<UserInfoStatsMobile stats={stats} />);
+  const wrapper = mount(<UserInfoStatsMobile totalPost={45} />);
 
   it('Should render Box', () => {
     render(<Box />);
   });
 
-  // TODO: fix tests
-  // it('Should render posts stat', () => {
-  //   expect(wrapper.find('#userInfoStatsPosts').hostNodes().html()).toBe(
-  //     '45'
-  //   );
-  //
-  // });
-  //
-  // it('Should render followers stat', () => {
-  //   expect(wrapper.find('#userInfoStatsFollowers').hostNodes().childAt(0).text()).toBe(
-  //     '450'
-  //   );
-  // });
-  //
-  // it('Should render followed accounts stat', () => {
-  //   expect(wrapper.find('#userInfoStatsFollowing').first().text()).toBe(
-  //     '789 following'
-  //   );
-  // });
+  it('Should render posts stat', () => {
+    expect(wrapper.find('#userInfoStatsPosts').hostNodes().text()).toBe(
+      '45posts'
+    );
+  });
+
 });
