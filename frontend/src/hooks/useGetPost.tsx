f3111f0e2121a514257da9e71dc702a1ed608e7f
Maxime Caron <maximecaron321@gmail.com>
2021-02-18 15:45:49 -0500

    fixes mistake when resolving conflict in useGetPost

--- frontend/src/hooks/useGetPost.tsx
+++ frontend/src/hooks/useGetPost.tsx
@@ -4,7 +4,7 @@ import useAPI from './useAPI';
 
 export default function useGetPost(postId: string) {
   const [post, setPost] = useState<Post>();
-  const [isLoading, error] = useAPI('getPost', setPost, postId);
+  const { isLoading, error } = useAPI('getPost', setPost, postId);
 
   // TODO : Use everything in the hook or remove them
   return { post, isLoading, error };
