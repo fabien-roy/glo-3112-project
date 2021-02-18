2c8e160e37977988f617e56c698813d8746829c0
Omar <omar.akrout.1@ulaval.ca>
2021-02-18 17:41:24 -0500

    fix transpile react-s3 + add test to EditUserAvatar

--- frontend/src/components/users/EditUserAvatar.test.tsx
+++ frontend/src/components/users/EditUserAvatar.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { UserAvatar } from './UserAvatar';
 import { EditUserAvatar } from './EditUserAvatar';
+import useUploadToS3 from 'hooks/images/useUploadToS3';
 
 const props = {
-  username: 'Test',
-  onUpload: jest.fn(),
+  username: 'Test'
 };
 
 // TODO: validate when file changes useUploadToS3 hook is called
@@ -31,9 +31,31 @@ const props = {
 //   });
 // });
 
+jest.mock('hooks/images/useUploadToS3');
+
+const useUploadHookResponse = ['reference', null];
+
 describe('When rendering EditUserAvatar', () => {
+  beforeEach(() => {
+    useUploadToS3.mockReturnValue(useUploadHookResponse);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Should call useUploadToS3 Hook with the right file', () => {
+    const wrapper = mount(<EditUserAvatar {...props} />);
+    const files = ['someFiles'];
+    const input = wrapper.find('input');
+
+    input.simulate('change', { target: { files } });
+
+    expect(useUploadToS3).toHaveBeenCalledWith(files[0], 'avatars');
+  });
+
   it('Should render UserAvatar', () => {
-    const wrapper = shallow(<EditUserAvatar {...props} />);
+    const wrapper = mount(<EditUserAvatar {...props} />);
 
     expect(wrapper.containsMatchingElement(<UserAvatar {...props} />)).toEqual(
       true
