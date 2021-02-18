d0bacb2ddf9579b2786166138f80605995f93adc
Omar <omar.akrout.1@ulaval.ca>
2021-02-18 17:57:51 -0500

    fix env variable to react nomination

--- frontend/src/services/S3Service.tsx
+++ frontend/src/services/S3Service.tsx
@@ -1,10 +1,10 @@
 import S3 from 'react-s3';
 
 const config = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  bucketName: process.env.S3_BUCKET_NAME,
-  region: process.env.S3_BUCKET_REGION,
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  bucketName: process.env.REACT_APP_S3_BUCKET_NAME,
+  region: process.env.REACT_APP_S3_BUCKET_REGION,
 };
 
 const uploadFile = (file, dirName) => {
