2483fe4f936b381ba00b9e4079d803573b727b15
Annie Auclair <annie@shimadzu.ca>
2021-02-17 20:58:38 -0500

    fixed filtering issues

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -2,11 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { InputAdornment } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { User } from 'types/users';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import useGetUsers from '../hooks/useGetUsers';
 
 const useStyles = makeStyles(() => ({
@@ -18,58 +18,44 @@ const useStyles = makeStyles(() => ({
 
 export function SearchBar() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  let options: User[] = [];
-  let value: User | null = null;
-  let loading = false;
-  const [users] = useGetUsers();
-
-  if (!open) {
-    options = [];
-  } else if (Array.isArray(users) && users.length > 0) {
-    options = Object.keys(users).map((key) => users[key]) as User[];
-  } else {
-    loading = true;
-  }
+  const [users, IsLoading] = useGetUsers();
 
   const history = useHistory();
+
+  let options: User[] = [];
+  const value: User | null = null;
+
+  if (Array.isArray(users) && users.length > 0) {
+    options = Object.keys(users).map((key) => users[key]) as User[];
+  }
+
   const handleInputChange = (username: string) => {
     const currentUserName = username;
     if (currentUserName !== '') {
       const currenRoute = `/users/${currentUserName}`;
       history.push(currenRoute);
       options = [];
-      value = null;
     }
   };
+
   return (
     <Autocomplete
-      id="user-dropdown"
+      id="search-user"
       style={{ width: 300 }}
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
+      options={options}
+      autoHighlight
+      getOptionLabel={(option) => option.username}
       value={value}
+      clearOnEscape
       onChange={(event: any, newValue: User | null) => {
-        value = newValue;
         if (newValue) {
           handleInputChange(newValue.username);
         }
       }}
-      inputValue=""
-      getOptionLabel={(option) => option.username}
-      options={options}
-      loading={loading}
-      blurOnSelect
-      autoHighlight
       renderInput={(params) => (
         <TextField
           {...params}
-          id="outlined-full-width"
+          id="search-input"
           style={{ margin: 8 }}
           placeholder="Search user"
           fullWidth
@@ -78,19 +64,19 @@ export function SearchBar() {
           variant="outlined"
           InputProps={{
             ...params.InputProps,
-            endAdornment: (
-              <>
-                {loading ? (
-                  <CircularProgress color="inherit" size={20} />
-                ) : null}
-                {params.InputProps.endAdornment}
-              </>
-            ),
             className: classes.input,
             startAdornment: (
               <InputAdornment position="start">
                 <SearchIcon />
               </InputAdornment>
+            ),
+            endAdornment: (
+              <>
+                {IsLoading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
             ),
           }}
         />
