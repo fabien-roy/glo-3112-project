40566b90c509777d2f9dfa8b50d69c7e963f205e
Maxime Caron <maximecaron321@gmail.com>
2021-02-18 20:35:36 -0500

    fix the component attribute of PostImage causing two images stacked on top of eachother, also suppresses the warning when testing

--- frontend/src/components/posts/PostImage.tsx
+++ frontend/src/components/posts/PostImage.tsx
@@ -27,8 +27,7 @@ export const PostImage: React.FC<PostImageProps> = (props: PostImageProps) => {
     <CardMedia
       className={paddingClass === 'wide' ? classes.wide : classes.small}
       image={reference}
-      component="img"
-      alt="Post image"
+      component="div"
     />
   );
 };
