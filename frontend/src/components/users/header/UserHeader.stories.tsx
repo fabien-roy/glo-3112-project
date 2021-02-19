eaa487f65f711415fbb31ff69b585eac9b79cc60
Fabien Roy <fabienroy28@gmail.com>
2021-02-19 13:22:26 -0500

    Add stories to UserHeader

--- frontend/src/components/users/header/UserHeader.stories.tsx
+++ frontend/src/components/users/header/UserHeader.stories.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import { UserFactory } from 'factories/UserFactory';
 import { UserHeader, UserHeaderProps } from './UserHeader';
+
+const user = UserFactory.make();
+const stats = {
+  totalPost: 45,
+};
 
 export default {
   title: 'components/users/header/UserHeader',
@@ -13,22 +19,17 @@ const Template: Story<UserHeaderProps> = ({ ...args }) => (
 
 export const WithoutAvatar = Template.bind({});
 WithoutAvatar.args = {
-  username: 'Test',
-  stats: {
-    totalPost: 52,
-  },
-  fullname: 'Full Name',
-  description: 'This is a description',
+  username: user.username,
+  fullname: user.firstName,
+  description: user.description,
+  stats,
 };
 
 export const WithAvatar = Template.bind({});
 WithAvatar.args = {
-  username: 'Test',
-  avatarSrc:
-    'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
-  stats: {
-    totalPost: 52,
-  },
-  fullname: 'Full Name',
-  description: 'This is a description',
+  username: user.username,
+  fullname: user.firstName,
+  description: user.description,
+  avatarSrc: user.avatarReference,
+  stats,
 };
