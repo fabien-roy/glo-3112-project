557ae664106f5a5e45818eb0e058190532ac6730
Fabien Roy <fabienroy28@gmail.com>
2021-02-19 13:14:21 -0500

    Add story to UserInfoHeader

--- frontend/src/components/users/info/UserInfoHeader.test.tsx
+++ frontend/src/components/users/info/UserInfoHeader.test.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { mount } from 'enzyme';
 import Box from '@material-ui/core/Box';
+import { UserFactory } from 'factories/UserFactory';
 import { UserInfoHeader } from './UserInfoHeader';
+
+const user = UserFactory.make();
 
 describe('When rendering UserInfoHeader', () => {
   it('Should render Box', () => {
@@ -10,10 +13,10 @@ describe('When rendering UserInfoHeader', () => {
   });
 
   it('Should render passed username', () => {
-    const wrapper = mount(<UserInfoHeader username="Username" />);
+    const wrapper = mount(<UserInfoHeader username={user.username} />);
 
     expect(wrapper.find('#UserInfoHeaderUsername').hostNodes().text()).toBe(
-      'Username'
+      user.username
     );
   });
 });
