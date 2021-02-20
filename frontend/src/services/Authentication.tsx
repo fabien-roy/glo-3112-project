42e12c2482ae5c872b14b2a9b5a4d35a4f60dcf5
Annie Auclair <annie@shimadzu.ca>
2021-02-20 12:02:46 -0500

    Part of Implement current user #193 (logged user is 1st user from the User array);  global function to get loggedUser

--- frontend/src/services/Authentication.tsx
+++ frontend/src/services/Authentication.tsx
@@ -1,15 +1,19 @@
 import { User } from 'types/users';
 
+let loggedUser: User = {
+  username: 'TestUser',
+  email: '',
+  phoneNumber: '',
+  firstName: 'Test',
+  lastName: 'User',
+  description: '',
+  avatarReference: '',
+};
+
+export function setLoggedUser(user: User) {
+  loggedUser = user;
+}
+
 export function getLoggedUser() {
-  const user: User = {
-    username: 'TestUser',
-    email: '',
-    phoneNumber: '',
-    firstName: 'Test',
-    lastName: 'User',
-    description: '',
-    avatarReference:
-      'https://secure.gravatar.com/avatar/9f1f9255ae409c09a725b269b586405a',
-  };
-  return user;
+  return loggedUser;
 }
