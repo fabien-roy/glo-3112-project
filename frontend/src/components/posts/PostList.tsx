636047d798ce3a5311d35cd9f612302f8747bf66
Fabien Roy <fabienroy28@gmail.com>
2021-02-20 14:56:25 -0500

    Simplify PostList

--- frontend/src/components/posts/PostList.tsx
+++ frontend/src/components/posts/PostList.tsx
@@ -4,28 +4,26 @@ import Box from '@material-ui/core/Box';
 import { Post } from 'types/posts';
 import { PostCard } from './PostCard';
 
+// TODO : Add test for PostList
+// TODO : Add story for PostList
 export function PostList(props: { posts: Post[] }) {
   const { posts } = props;
-
-  const list = posts.map((post) => {
-    return (
-      <Grid item key={post._id} xs={12} md={4}>
-        <PostCard
-          id={post._id}
-          description={post.description}
-          reference={post.reference}
-          hashtags={post.hashtags}
-          usertags={post.usertags}
-          user={post.user}
-        />
-      </Grid>
-    );
-  });
 
   return (
     <Box mt={2}>
       <Grid container spacing={2}>
-        {list}
+        {posts.map((post) => (
+          <Grid item key={post._id} xs={12} md={4}>
+            <PostCard
+              id={post._id}
+              description={post.description}
+              reference={post.reference}
+              hashtags={post.hashtags}
+              usertags={post.usertags}
+              user={post.user}
+            />
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
