08454c795431285f8026208761c19b5090a275f4
Fabien Roy <fabienroy28@gmail.com>
2021-02-21 17:27:07 -0500

    Fix useFetchFromAPI

--- frontend/src/hooks/useFetchFromAPI.tsx
+++ frontend/src/hooks/useFetchFromAPI.tsx
@@ -5,7 +5,7 @@ export default function useFetchFromAPI(method, setData, ...params) {
   const { act, isLoading, error } = useActOnAPI(method, setData, params);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => act(), [...params]);
+  useEffect(() => act({}), [...params]);
 
   return { isLoading, error };
 }
