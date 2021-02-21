220d24e8bbcbec748d64c23c00ea8a895d14fd94
Fabien Roy <fabienroy28@gmail.com>
2021-02-21 16:41:23 -0500

    Add form inputs

---
+++ frontend/src/components/forms/ImageField.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { FieldProps, getIn } from 'formik';
+import { FormControl, IconButton } from '@material-ui/core';
+
+// TODO : Use label and placeholder or remove them
+interface FormImageUploadFieldProps extends FieldProps {
+  label: string;
+  placeholder: string;
+  test: (File) => void;
+  handleChange: (event) => void;
+}
+
+// TODO : Write tests for ImageField
+// TODO : Write stories for ImageField
+export const FormImageUploadField: React.FC<FormImageUploadFieldProps> = ({
+  field,
+  form,
+  ...props
+}) => {
+  const [reference, setReference] = useState<string | ArrayBuffer | null>();
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    if (!event.target.files) {
+      return;
+    }
+    const newFile = event.target.files[0];
+    props.test(newFile);
+    const reader = new FileReader();
+
+    if (newFile) {
+      reader.onloadend = () => {
+        setReference(reader.result);
+      };
+      reader.readAsDataURL(newFile);
+    }
+  };
+
+  const errorText =
+    getIn(form.touched, field.name) && getIn(form.errors, field.name);
+  return (
+    <FormControl fullWidth error={!!errorText}>
+      <label htmlFor="icon-button-file">
+        <input
+          name="file"
+          type="file"
+          onChange={(event) => {
+            handleImageChange(event);
+            props.handleChange(event);
+          }}
+        />
+        <IconButton
+          color="primary"
+          aria-label="upload picture"
+          component="span"
+        />
+      </label>
+      {typeof reference === 'string' && <img src={reference} alt="" />}
+      <FormControl>{errorText}</FormControl>
+    </FormControl>
+  );
+};
+
+export default FormImageUploadField;
---
+++ frontend/src/components/forms/TextField.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FormControl, TextField } from '@material-ui/core';
+import { FieldProps, getIn } from 'formik';
+
+interface FormFieldProps extends FieldProps {
+  label: string;
+  placeholder: string;
+  multiline?: boolean;
+  rows?: number;
+}
+
+// TODO : Write tests for TextField
+// TODO : Write stories for TextField
+export const FormTextField: React.FC<FormFieldProps> = ({
+  field,
+  form,
+  ...props
+}) => {
+  const { label, placeholder, multiline, rows } = props;
+  const errorText =
+    getIn(form.touched, field.name) && getIn(form.errors, field.name);
+  return (
+    <FormControl fullWidth error={!!errorText}>
+      <TextField
+        color="primary"
+        rows={rows || 1}
+        multiline={multiline || false}
+        label={label}
+        placeholder={placeholder}
+        {...field}
+      />
+      <FormControl>{errorText}</FormControl>
+    </FormControl>
+  );
+};
+
+export default FormTextField;
