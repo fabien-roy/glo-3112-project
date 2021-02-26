aa51192b53258654b2895698d7f6aa937cd970cf
Fabien Roy <fabienroy28@gmail.com>
2021-02-26 11:00:18 -0500

    Try new db config

--- backend/src/mongo.options.ts
+++ backend/src/mongo.options.ts
@@ -1,20 +1,19 @@
 import fs from 'fs';
 const certFileBuf = [fs.readFileSync('./rds-combined-ca-bundle.pem')];
 
-export const mongoOptions = process.env.NODE_ENV === 'production' ? {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  user: process.env.MONGO_USERNAME,
-  pass: process.env.MONGO_PASSWORD,
-  ssl: true,
-  sslCA: certFileBuf,
-  replicateSet: 'rs0',
-  retryWrite: false,
-} : {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  user: process.env.MONGO_USERNAME,
-  pass: process.env.MONGO_PASSWORD,
-};
+export const mongoOptions =
+  process.env.NODE_ENV === 'production'
+    ? {
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true,
+        sslCA: certFileBuf,
+      }
+    : {
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        user: process.env.MONGO_USERNAME,
+        pass: process.env.MONGO_PASSWORD,
+      };
