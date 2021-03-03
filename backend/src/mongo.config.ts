3b55d5a7e3f49416c5a00bab1afe14160121c498
Fabien Roy <fabienroy28@gmail.com>
2021-03-03 09:48:04 -0500

    Try MongoDB Atlas

--- backend/src/mongo.config.ts
+++ backend/src/mongo.config.ts
@@ -1,28 +1,15 @@
-import fs from 'fs';
-const certFileBuf = [fs.readFileSync('./rds-combined-ca-bundle.pem')];
-
-const baseMongoURL = process.env.MONGO_URL || '';
+const baseMongoURL = process.env.MONGO_URL || "";
 
 export const mongoURL =
-  process.env.NODE_ENV === 'production'
-    ? `${baseMongoURL}?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred`
+  process.env.NODE_ENV === "production"
+    ? `${baseMongoURL}?retryWrites=true&w=majority`
     : baseMongoURL;
 
 export const mongoOptions =
-  process.env.NODE_ENV === 'production'
-    ? {
-        sslValidate: true,
-        sslCA: certFileBuf,
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        user: process.env.MONGO_USERNAME,
-        pass: process.env.MONGO_PASSWORD,
-      }
-    : {
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        user: process.env.MONGO_USERNAME,
-        pass: process.env.MONGO_PASSWORD,
-      };
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD
+  };
