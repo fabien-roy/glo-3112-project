92164a570348ad7529c9765983ba5100ef8f4596
Maxime Caron <maximecaron321@gmail.com>
2021-03-05 16:19:51 -0500

    wait for every part to finish loading before rendering

--- frontend/src/views/posts/FeedView.tsx
+++ frontend/src/views/posts/FeedView.tsx
@@ -44,7 +44,7 @@ export const FeedView = () => {
 
   return (
     <>
-      {content}
+      {!getPostsIsLoading && !getLoggedUserIsLoading && content}
       {errorMessage}
       {loading}
     </>
