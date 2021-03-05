30e5201193e3dbccbb778d0ac45e4158d29fab3d
Fabien Roy <fabienroy28@gmail.com>
2021-03-05 14:39:22 -0500

    Fix lint

--- backend/src/connect.database.ts
+++ backend/src/connect.database.ts
@@ -1,41 +1,40 @@
-import mongoose from "mongoose";
-import { FakeDataGenerator } from "./generators/fake.data.generator";
-import { logger } from "./logger";
+import mongoose from 'mongoose';
+import { FakeDataGenerator } from './generators/fake.data.generator';
+import { logger } from './logger';
 
-export const mongoURL = process.env.MONGO_URL || "";
+export const mongoURL = process.env.MONGO_URL || '';
 
-export const mongoOptions =
-  {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD
-  };
+export const mongoOptions = {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: process.env.MONGO_USERNAME,
+  pass: process.env.MONGO_PASSWORD,
+};
 
 // const retryTimeoutInMilliseconds = 5000;
 
 const db = mongoose.connection;
 
-db.on("connecting", () => logger.info("Connecting to MongoDB..."));
+db.on('connecting', () => logger.info('Connecting to MongoDB...'));
 
-db.on("error", (error) =>
-  logger.error(`Error in MongoDB connection : ${error}`)
+db.on('error', (error) =>
+  logger.error(`Error in MongoDB connection : ${error}`),
 );
-db.on("connected", () => logger.info("MongoDB connected!"));
+db.on('connected', () => logger.info('MongoDB connected!'));
 
-db.once("open", async () => {
-  logger.info("MongoDB connection opened!");
+db.once('open', async () => {
+  logger.info('MongoDB connection opened!');
 
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV !== 'production') {
     await new FakeDataGenerator().generateIfEmpty();
   }
 });
 
-db.on("reconnected", () => logger.info("MongoDB reconnected!"));
+db.on('reconnected', () => logger.info('MongoDB reconnected!'));
 
-db.on("disconnected", () => {
-  logger.info("MongoDB disconnected!");
+db.on('disconnected', () => {
+  logger.info('MongoDB disconnected!');
   // retryConnectionAfterTimeout();
 });
 
