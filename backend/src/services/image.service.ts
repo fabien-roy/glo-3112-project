01fa394dcf0036e44ef20a5cd4c91256cd00bc01
Fabien Roy <fabienroy28@gmail.com>
2021-03-08 19:40:17 -0500

    Validate data type

--- backend/src/services/image.service.ts
+++ backend/src/services/image.service.ts
@@ -1,18 +1,29 @@
 import { S3Client } from '../clients/s3.client';
+import { BadRequestError } from '../types/errors';
 
 // TODO : Use winston to log some things
 export class ImageService {
   private s3Client: S3Client = new S3Client();
 
   public async uploadAvatar(avatarData: string): Promise<string> {
-    const buffer = this.toBuffer(avatarData);
-    // TODO : Validate if buffer is an image
-    // TODO : Validate if buffer is correct file size
-    // TODO : Validate if buffer is correct file type
+    ImageService.validateDataType(avatarData);
+    // TODO : Validate if data is correct file size
+
+    const buffer = ImageService.toBuffer(avatarData);
     return this.s3Client.uploadAvatar(buffer);
   }
 
-  private toBuffer(data: string): Buffer {
+  // TODO : Test this
+  private static validateDataType(data: string) {
+    // TODO : Check if png and jpeg are the only accepted types
+    const dataTypeRegex = /^data:image\/(?:png|jpeg)(?:;charset=utf-8)?;base64,(?:[A-Za-z0-9]|[+/])+={0,2}/g;
+
+    if (!dataTypeRegex.test(data)) {
+      throw new BadRequestError('Invalid image type');
+    }
+  }
+
+  private static toBuffer(data: string): Buffer {
     return Buffer.from(data, 'base64');
   }
 }
