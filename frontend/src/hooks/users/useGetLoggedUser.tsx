d63505208ebfe81370d249fba1a5daec6756773b
Omar <omar.akrout.1@ulaval.ca>
2021-03-15 17:54:59 -0400

    delete useless imports

--- frontend/src/hooks/users/useGetLoggedUser.tsx
+++ frontend/src/hooks/users/useGetLoggedUser.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { User } from 'types/users';
 import useFetchFromAPI from 'hooks/useFetchFromAPI';
 
-// TODO : Refactor this hook to only consider logged user
 export default function useGetLoggedUser() {
   const [loggedUser, setLoggedUser] = useState<User | null>(null);
 
