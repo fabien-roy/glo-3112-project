5045649d2af5fc4bd383605d0b83b61bb9c37b87
Omar <omar.akrout.1@ulaval.ca>
2021-03-16 11:14:29 -0400

    fix frontend lint

--- frontend/src/components/users/settings/EditUserFormButtons.test.tsx
+++ frontend/src/components/users/settings/EditUserFormButtons.test.tsx
@@ -6,43 +6,42 @@ import { EditUserFormButtons } from './EditUserFormButtons';
 
 const props = {
   disableSend: true,
-  delete: () => {console.log("delete account")},
-}
+  delete: () => {
+    console.log('delete account');
+  },
+};
 
-const sendButton = <Button
-  disabled={props.disableSend}
-  variant="contained"
-  color="primary"
-  type="submit"
->
-  Send
-</Button>
+const sendButton = (
+  <Button
+    disabled={props.disableSend}
+    variant="contained"
+    color="primary"
+    type="submit"
+  >
+    Send
+  </Button>
+);
 
-const deleteAccountButton =
-<Button variant="outlined" color="primary" onClick={props.delete}>
-  Delete your account
-</Button>
+const deleteAccountButton = (
+  <Button variant="outlined" color="primary" onClick={props.delete}>
+    Delete your account
+  </Button>
+);
 
 describe('When rendering EditUserFormButtons', () => {
   it('Should render delete account Button with passed delete prop', () => {
-    const wrapper = mount(
-      <EditUserFormButtons {...props} />
-    );
+    const wrapper = mount(<EditUserFormButtons {...props} />);
 
     expect(wrapper.containsMatchingElement(deleteAccountButton)).toBeTruthy();
   });
 
   it('Should render send Button with passed disabled prop', () => {
-    const wrapper = mount(
-      <EditUserFormButtons {...props} />
-    );
+    const wrapper = mount(<EditUserFormButtons {...props} />);
 
     expect(wrapper.containsMatchingElement(sendButton)).toBeTruthy();
   });
 
   it('Should render', () => {
-    render(
-      <EditUserFormButtons {...props} />
-    );
+    render(<EditUserFormButtons {...props} />);
   });
 });
