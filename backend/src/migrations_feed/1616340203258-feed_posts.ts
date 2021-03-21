90b533a11876a97d5ae6e7492c83281522c433b8
Fabien Roy <fabienroy28@gmail.com>
2021-03-21 12:45:24 -0400

    Fix usage of spread operator

--- backend/src/migrations_feed/1616340203258-feed_posts.ts
+++ backend/src/migrations_feed/1616340203258-feed_posts.ts
@@ -12,9 +12,8 @@ exports.up = async (done: any) => {
   const insertOperations: Array<any> = [];
 
   fakeUsers.forEach((fakeUser) => {
-    insertOperations.push.apply(
-      insertOperations,
-      insertManyFakeDocuments(
+    insertOperations.push(
+      ...insertManyFakeDocuments(
         Posts,
         PostFactory.make(AMOUNT_OF_POSTS_TO_FEED_PER_USER, {
           user: fakeUser.username,
