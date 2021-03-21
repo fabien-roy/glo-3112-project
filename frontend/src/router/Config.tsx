fdc24acc32652859671182be22f531fae07d59b0
Omar <omar.akrout.1@ulaval.ca>
2021-03-21 12:45:47 -0400

    add router paths constants

--- frontend/src/router/Config.tsx
+++ frontend/src/router/Config.tsx
@@ -5,55 +5,58 @@ export const ROUTE_PATHS = {
   home: '/',
   login: '/login',
   feed: '/posts',
-  
-}
+  post: '/posts/:postId',
+  user: '/users/:username',
+  settings: '/settings',
+  search: '/search',
+};
 
 export const routes: RouteProps[] = [
   {
     key: 'root',
-    path: '/',
+    path: ROUTE_PATHS.home,
     exact: true,
     redirect: '/posts',
     private: true,
   },
   {
     key: 'login',
-    path: '/login',
+    path: ROUTE_PATHS.login,
     component: lazy(() => import('views/auth/LoginView')),
     exact: true,
     private: false,
   },
   {
     key: 'feed',
-    path: '/posts',
+    path: ROUTE_PATHS.feed,
     component: lazy(() => import('views/posts/FeedView')),
     exact: true,
     private: true,
   },
   {
     key: 'post',
-    path: '/posts/:postId',
+    path: ROUTE_PATHS.post,
     component: lazy(() => import('views/posts/PostView')),
     exact: true,
     private: true,
   },
   {
     key: 'user',
-    path: '/users/:username',
+    path: ROUTE_PATHS.user,
     component: lazy(() => import('views/users/UserView')),
     exact: true,
     private: true,
   },
   {
     key: 'settings',
-    path: '/settings',
+    path: ROUTE_PATHS.settings,
     component: lazy(() => import('views/settings/SettingsView')),
     exact: true,
     private: true,
   },
   {
     key: 'search',
-    path: '/search',
+    path: ROUTE_PATHS.search,
     component: lazy(() => import('views/search/SearchView')),
     exact: true,
     private: true,
