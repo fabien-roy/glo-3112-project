85917d60b767ed8e11c83768bd6e743c4bfc4df1
Maxime Caron <maximecaron321@gmail.com>
2021-04-10 03:27:54 -0400

    fixes comment text responsiveness

--- frontend/src/components/posts/Comment.tsx
+++ frontend/src/components/posts/Comment.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { UserComment } from 'types/posts';
-import { ListItemAvatar, ListItemText, ListItem } from '@material-ui/core';
+import {
+  ListItemAvatar,
+  ListItemText,
+  ListItem,
+  Box,
+  makeStyles,
+  createStyles,
+} from '@material-ui/core';
 import { UserAvatar } from 'components/users/avatar/UserAvatar';
 
 export interface CommentProps {
   comment: UserComment;
 }
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    commentPrimaryText: {
+      wordBreak: 'break-word',
+    },
+  })
+);
+
 export const Comment: React.FC<CommentProps> = (props: CommentProps) => {
   const { comment } = props;
+  const classes = useStyles();
 
   return (
     <ListItem>
@@ -20,6 +36,8 @@ export const Comment: React.FC<CommentProps> = (props: CommentProps) => {
         />
       </ListItemAvatar>
       <ListItemText
+        className={classes.commentPrimaryText}
+        primaryTypographyProps={{}}
         primary={comment.text}
         secondary={`${comment.user}, on ${new Date(
           comment?.createdAt
