3730dfd974412f07965cc3b4bdcbbd13146cf207
Fabien Roy <fabienroy28@gmail.com>
2021-04-25 12:36:03 -0400

    Separate SearchListHashtags from SearchList

--- frontend/src/components/search/SearchList.tsx
+++ frontend/src/components/search/SearchList.tsx
@@ -10,16 +10,14 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { UserQueryParams } from 'types/users';
 import { ROUTE_PATHS } from 'router/Config';
-import { Avatar, useMediaQuery } from '@material-ui/core';
 import { purple } from '@material-ui/core/colors';
 import useGetUsers from 'hooks/users/useGetUsers';
 import useQuery from 'hooks/useQuery';
 import _ from 'lodash';
 import { UserAvatar } from '../users/avatar/UserAvatar';
-import { HashtagQueryParams } from '../../types/hashtags';
-import useGetHashtags from '../../hooks/hashtags/useGetHashtags';
 import LoadingSpinner from '../LoadingSpinner';
 import SearchListPosts from './SearchListPosts';
+import SearchListHashtags from './SearchListHashtags';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -70,10 +68,6 @@ export interface SearchListProps {
   setListRef?: any;
 }
 
-const getHashtagQueryParams = (searchValue: string): HashtagQueryParams => ({
-  like: searchValue || undefined,
-});
-
 const getUserQueryParams = (
   searchValue: string,
   after: string,
@@ -100,8 +94,6 @@ const SearchList: React.FC<SearchListProps> = (props: SearchListProps) => {
   );
   const [fetchedUsers, setFetchedUsers] = useState(users.results);
 
-  const { hashtags } = useGetHashtags(getHashtagQueryParams(searchValue));
-
   useEffect(() => {
     setListRef(listRef);
   });
@@ -127,11 +119,10 @@ const SearchList: React.FC<SearchListProps> = (props: SearchListProps) => {
     history.push(newRoute);
   };
 
-  const smallMobile = useMediaQuery('(max-width:400px)');
   return (
     <div>
       <div>
-        {tab < 2 && (
+        {tab === 0 && (
           <TableContainer className={classes.table} component={Paper}>
             <div
               id="scrollTable"
@@ -146,80 +137,48 @@ const SearchList: React.FC<SearchListProps> = (props: SearchListProps) => {
                 loader={<LoadingSpinner key={0} />}
               >
                 <Table className={classes.table} aria-label="simple table">
-                  {tab === 0 && (
-                    <TableBody>
-                      {fetchedUsers.map((row) => (
-                        <TableRow
-                          className={classes.tableRow}
-                          key={row.username}
-                          onClick={() =>
-                            handleClick(ROUTE_PATHS.user(row.username))
-                          }
+                  <TableBody>
+                    {fetchedUsers.map((row) => (
+                      <TableRow
+                        className={classes.tableRow}
+                        key={row.username}
+                        onClick={() =>
+                          handleClick(ROUTE_PATHS.user(row.username))
+                        }
+                      >
+                        <TableCell
+                          className={classes.tableCell}
+                          align="left"
+                          width="10%"
                         >
-                          <TableCell
-                            className={classes.tableCell}
-                            align="left"
-                            width="10%"
-                          >
-                            <UserAvatar
-                              src={tab === 0 ? row.avatarReference : '#'}
-                              size="small"
-                              username={row.username}
-                            />
-                          </TableCell>
-                          <TableCell
-                            className={classes.tableCell}
-                            align="left"
-                            width="30%"
-                          >
-                            {row.username}
-                          </TableCell>
-                          <TableCell className={classes.tableCell} align="left">
-                            <div className={classes.sectionDesktop}>
-                              {`${row.firstName} ${row.lastName}`}
-                            </div>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  )}
-                  {tab === 1 && (
-                    <TableBody>
-                      {hashtags.map((row) => (
-                        <TableRow
-                          key={row.name}
-                          className={classes.tableRow}
-                          onClick={() =>
-                            handleClick(ROUTE_PATHS.feed(`hashtag=${row.name}`))
-                          }
+                          <UserAvatar
+                            src={tab === 0 ? row.avatarReference : '#'}
+                            size="small"
+                            username={row.username}
+                          />
+                        </TableCell>
+                        <TableCell
+                          className={classes.tableCell}
+                          align="left"
+                          width="30%"
                         >
-                          <TableCell
-                            className={classes.tableCell}
-                            align="left"
-                            width="10%"
-                          >
-                            <Avatar>{tab === 1 && '#'}</Avatar>
-                          </TableCell>
-                          <TableCell
-                            className={classes.tableCell}
-                            align="left"
-                            width={smallMobile ? '20%' : '30%'}
-                          >
-                            {row.name}
-                          </TableCell>
-                          <TableCell className={classes.tableCell} align="left">
-                            {row.count === 1 ? '1 post' : `${row.count} posts`}
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  )}
+                          {row.username}
+                        </TableCell>
+                        <TableCell className={classes.tableCell} align="left">
+                          <div className={classes.sectionDesktop}>
+                            {`${row.firstName} ${row.lastName}`}
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
                 </Table>
               </InfiniteScroll>
             </div>
           </TableContainer>
         )}
       </div>
+      <div>{tab === 1 && <SearchListHashtags />}</div>
       <div>{tab === 2 && <SearchListPosts />}</div>
     </div>
   );
